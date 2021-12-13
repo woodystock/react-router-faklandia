76a583c2c5bf99136f661f4beefff48adb8a550c
woodystock <91628945+woodystock@users.noreply.github.com>
2021-12-13 00:29:55 +0000

    Added Nested Routes

---
+++ src/layouts/FaklandiaLayout.js
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom"
+
+const FaklandiaLayout = () => {
+    return (
+        <div>
+            Layout
+            <Outlet />
+        </div>
+    )
+}
+
+
+export default FaklandiaLayout
--- src/routes/FaklandiaRouter.js
+++ src/routes/FaklandiaRouter.js
@@ -3,14 +3,17 @@ import Confess from './Confess'
 import FaklandiaHome from './FaklandiaHome'
 import InvalidPath from './InvalidPath'
 import Misdemeaners from './Misdemeanours'
+import FaklandiaLayout from '../layouts/FaklandiaLayout'
 
 const FaklandiaRouter = () => {
     return (
         <Routes>
-            <Route path="/" element={<FaklandiaHome />} />
-            <Route path="/Misdemeanours" element={<Misdemeaners />} />
-            <Route path="/Confess" element={<Confess />} />
-            <Route path="*" element={<InvalidPath />} />
+            <Route path="/" element={<FaklandiaLayout />} >
+                <Route index element={<FaklandiaHome />} />
+                <Route path="/Misdemeanours" element={<Misdemeaners />} />
+                <Route path="/Confess" element={<Confess />} />
+                <Route path="*" element={<InvalidPath />} />
+            </Route>
         </Routes>
     )
 }
