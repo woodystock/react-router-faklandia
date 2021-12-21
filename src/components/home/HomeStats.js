6732aa118557e591ef79f179c17e9db59b7b9827
woodystock <91628945+woodystock@users.noreply.github.com>
2021-12-21 23:07:33 +0000

    Added a home page

--- src/components/home/HomeStats.js
+++ src/components/home/HomeStats.js
@@ -1,8 +1,27 @@
+import { useContext } from "react"
+
+import { MisdemeanersContext } from "../../context/MisdemeanoursContext"
+
 const HomeStats = () => {
+
+    const [misdemeanours] = useContext(MisdemeanersContext)
+
+    const getLastConfession = () => {
+        if(misdemeanours.length === 0)
+            return "";
+
+        for( var i = misdemeanours.length - 1; i >= 0; i--)
+        {
+            if(misdemeanours[i].confession)
+                return misdemeanours[i].confession;
+        }
+
+        return "";
+    }
+
     return <div className="stats-container">
-        <p>Total Misdemeanours: </p>
-        <p>It has been 0 days since the last misdemeanour</p>
-        <p>Most recent confession:</p>
+        <p className="stats-text">Total Misdemeanours: {misdemeanours.length} </p>
+        <p className="stats-text">Most recent confession: </p>{getLastConfession()}
     </div>
 }
 export default HomeStats
